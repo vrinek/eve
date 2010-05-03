29b4db9c19c44b72fa1d22fe05b752c3069dbd9a
Kostas Karachalios <kostas.karachalios@gmail.com>
2010-05-03 15:16:05 +0300

    added a welcome page + a logo

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Eve::Application.routes.draw do |map|
 
   get "tools/compare_items"
 
-  get "mining/calculator"
+  # get "mining/calculator"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -58,7 +58,7 @@ Eve::Application.routes.draw do |map|
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "tools#compare_items"
+  root :to => "welcome#home"
 
   # See how all your routes lay out with "rake routes"
 
