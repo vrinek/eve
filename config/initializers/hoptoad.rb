d452a0fb484483edc3d8bcd31ceee67b6016ebdf
Kostas Karachalios <kostas.karachalios@gmail.com>
2010-05-11 15:07:19 +0300

    removed hoptoad (again)

--- config/initializers/hoptoad.rb
+++ config/initializers/hoptoad.rb
@@ -1,3 +1,3 @@
-HoptoadNotifier.configure do |config|
-  config.api_key = '5557e12f8aa6377f7e4dcd76648d191a'
-end
+# HoptoadNotifier.configure do |config|
+#   config.api_key = '5557e12f8aa6377f7e4dcd76648d191a'
+# end
